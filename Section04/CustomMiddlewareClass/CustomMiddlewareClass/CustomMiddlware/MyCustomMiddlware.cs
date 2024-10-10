cf10f1ba38cf6d6c82098b3d2c9c2822f54cb0bb
kiran.gondi <kiran.gondi@planview.com>
2024-10-10 22:41:36 +0530

    Custom Middleware

---
+++ Section04/CustomMiddlewareClass/CustomMiddlewareClass/CustomMiddlware/MyCustomMiddlware.cs
@@ -0,0 +1,13 @@
+﻿
+namespace CustomMiddlewareClass.CustomMiddlware
+{
+    public class MyCustomMiddlware : IMiddleware
+    {
+        public async Task InvokeAsync(HttpContext context, RequestDelegate next)
+        {
+            await context.Response.WriteAsync("My custom middlware - STARTS");
+            await next(context);
+            await context.Response.WriteAsync("My custom middlware - ENDS");
+        }
+    }
+}
