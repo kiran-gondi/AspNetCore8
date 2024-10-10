3cab417bbe5dfcdbe5e845ac2bad8a89026c76ff
kiran.gondi <kiran.gondi@planview.com>
2024-10-10 22:50:24 +0530

    Custom Middleware Extension

---
+++ Section04/CustomMiddlewareExtension/CustomMiddlewareExtension/CustomMiddlware/MyCustomMiddlware.cs
@@ -0,0 +1,20 @@
+﻿namespace CustomMiddlewareExtension.CustomMiddlware
+{
+    public class MyCustomMiddlware : IMiddleware
+    {
+        public async Task InvokeAsync(HttpContext context, RequestDelegate next)
+        {
+            await context.Response.WriteAsync("My custom middlware - STARTS");
+            await next(context);
+            await context.Response.WriteAsync("My custom middlware - ENDS");
+        }
+    }
+
+    public static class CustomMiddlewareExtension
+    {
+        public static IApplicationBuilder UseMyCustomMiddlware(this IApplicationBuilder app)
+        {
+            return app.UseMiddleware<MyCustomMiddlware>();
+        }
+    }
+}
---
+++ Section04/CustomMiddlewareExtension/CustomMiddlewareExtension/Program.cs
@@ -0,0 +1,24 @@
+using CustomMiddlewareExtension.CustomMiddlware;
+
+var builder = WebApplication.CreateBuilder(args);
+builder.Services.AddTransient<MyCustomMiddlware>();
+var app = builder.Build();
+
+
+//MiddleWare1
+app.Use(async (HttpContext context, RequestDelegate next) =>
+{
+    await context.Response.WriteAsync("MW 01");
+    await next(context);
+});
+
+//MiddleWare2
+app.UseMyCustomMiddlware();
+
+//MiddleWare3
+app.Run(async (HttpContext context) =>
+{
+    await context.Response.WriteAsync("MW03");
+});
+
+app.Run();
