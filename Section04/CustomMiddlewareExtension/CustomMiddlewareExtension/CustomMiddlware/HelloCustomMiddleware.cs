a3e5bed6a1d3534cab6faa0fae871638eb9a2188
kiran.gondi <kiran.gondi@planview.com>
2024-10-10 22:56:53 +0530

    Custom Conventional Middleware

---
+++ Section04/CustomMiddlewareExtension/CustomMiddlewareExtension/CustomMiddlware/HelloCustomMiddleware.cs
@@ -0,0 +1,37 @@
+﻿namespace CustomMiddlewareExtension.CustomMiddlware
+{
+    // You may need to install the Microsoft.AspNetCore.Http.Abstractions package into your project
+    public class HelloCustomMiddleware
+    {
+        private readonly RequestDelegate _next;
+
+        public HelloCustomMiddleware(RequestDelegate next)
+        {
+            _next = next;
+        }
+
+        public async Task Invoke(HttpContext httpContext)
+        {
+            //before logic
+            if (httpContext.Request.Query.ContainsKey("firstName") && httpContext.Request.Query.ContainsKey("lastName"))
+            {
+                string fullName = httpContext.Request.Query["firstName"] + " " + httpContext.Request.Query["lastName"];
+                await httpContext.Response.WriteAsync(fullName);
+            }
+
+            await _next(httpContext);
+
+            //after logic
+            await httpContext.Response.WriteAsync("HelloCustomMiddleware - ENDS");
+        }
+    }
+
+    // Extension method used to add the middleware to the HTTP request pipeline.
+    public static class HelloCustomMiddlewareExtensions
+    {
+        public static IApplicationBuilder UseHelloCustomMiddleware(this IApplicationBuilder builder)
+        {
+            return builder.UseMiddleware<HelloCustomMiddleware>();
+        }
+    }
+}
