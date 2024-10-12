bab37c0d0ba85265385b95a70e1bdb9d92b08d80
kiran.gondi <kiran.gondi@planview.com>
2024-10-12 23:25:32 +0530

    FromRoute and FromQuery

--- Section06/ModelBindingExample/ModelBindingExample/Controllers/HomeController.cs
+++ Section06/ModelBindingExample/ModelBindingExample/Controllers/HomeController.cs
@@ -4,7 +4,7 @@ namespace ModelBindingExample.Controllers
 {
     public class HomeController : Controller
     {
-        [Route("bookstore/{bookid?}/{isLoggedin?}")]
+        /*[Route("bookstore/{bookid?}/{isLoggedin?}")]
         //URL: /bookstore?bookid=10&isloggedin=true
         public IActionResult Index(int? bookid, bool? isLoggedin)
         {
@@ -23,6 +23,34 @@ namespace ModelBindingExample.Controllers
             }
 
             if (isLoggedin == false) {
+                return StatusCode(401);
+            }
+
+            return Content($"Book id: {bookid}");
+        }*/
+
+        [Route("bookstore/{bookid?}/{isLoggedin?}")]
+        //URL: /bookstore?bookid=10&isloggedin=true
+        //public IActionResult Index([FromRoute]int? bookid, [FromRoute] bool? isLoggedin)
+        public IActionResult Index([FromQuery]int? bookid, [FromQuery] bool? isLoggedin)
+        {
+            if (bookid.HasValue == false)
+            {
+                return BadRequest("Book id is not supplied or empty");
+            }
+
+            if (bookid <= 0)
+            {
+                return BadRequest("Book id can't be less than or equal to 0");
+            }
+
+            if (bookid > 1000)
+            {
+                return NotFound("Book id can't be greater than 1000");
+            }
+
+            if (isLoggedin == false)
+            {
                 return StatusCode(401);
             }
 
