5f90f4a94f1a23c72b49f1f7ae7167f65785a801
kiran.gondi <kiran.gondi@planview.com>
2024-10-12 23:21:26 +0530

    Query String Vs Route Data

---
+++ Section06/ModelBindingExample/ModelBindingExample/Controllers/HomeController.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace ModelBindingExample.Controllers
+{
+    public class HomeController : Controller
+    {
+        [Route("bookstore/{bookid?}/{isLoggedin?}")]
+        //URL: /bookstore?bookid=10&isloggedin=true
+        public IActionResult Index(int? bookid, bool? isLoggedin)
+        {
+            if (bookid.HasValue == false)
+            {
+                return BadRequest("Book id is not supplied or empty");
+            }
+
+            if (bookid <= 0) {
+                return BadRequest("Book id can't be less than or equal to 0");
+            }
+
+            if(bookid > 1000)
+            {
+                return NotFound("Book id can't be greater than 1000");
+            }
+
+            if (isLoggedin == false) {
+                return StatusCode(401);
+            }
+
+            return Content($"Book id: {bookid}");
+        }
+    }
+}
