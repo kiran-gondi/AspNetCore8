05296f31eb15c7c8831eabc57d14fcc8e0f8f773
kiran.gondi <kiran.gondi@planview.com>
2024-10-12 17:51:20 +0530

    Status Code Result

--- Section06/IActionResultExample/IActionResultExample/Controllers/HomeController.cs
+++ Section06/IActionResultExample/IActionResultExample/Controllers/HomeController.cs
@@ -37,7 +37,7 @@ namespace IActionResultExample.Controllers
                     return File("/dummy.pdf", "application/pdf");
                 }*/
 
-        [Route("book")]
+        /*[Route("book")]
         public IActionResult Index()
         {
             if (!Request.Query.ContainsKey("bookid"))
@@ -72,6 +72,43 @@ namespace IActionResultExample.Controllers
             {
                 Response.StatusCode = 401;
                 return Content("User must be authenticated");
+            }
+
+            return File("/dummy.pdf", "application/pdf");
+        }*/
+
+        [Route("book")]
+        public IActionResult Index()
+        {
+            if (!Request.Query.ContainsKey("bookid"))
+            {
+                //return new BadRequestResult();
+                return BadRequest("Book id is not supplied.");
+            }
+
+            //Book id can't be empty
+            if (string.IsNullOrEmpty(Convert.ToString(Request.Query["bookid"])))
+            {
+                return BadRequest("Book id can't be null or empty.");
+            }
+
+            //Book id should be between 1 to 100
+            int bookId = Convert.ToInt32(ControllerContext.HttpContext.Request.Query["bookid"]);
+            if (bookId <= 0)
+            {
+                return BadRequest("Book id can't be less than or equal to zero.");
+            }
+
+            if (bookId > 1000)
+            {
+                return NotFound("Book id can't be greater than 1000");
+            }
+
+            //isLoggedin should be true
+            if (!Convert.ToBoolean(Request.Query["isLoggedin"]))
+            {
+                //return Unauthorized("User must be authenticated");
+                return StatusCode(401);
             }
 
             return File("/dummy.pdf", "application/pdf");
