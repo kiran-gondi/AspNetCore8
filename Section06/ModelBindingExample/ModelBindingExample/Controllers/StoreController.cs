2251e38ad8ee6af29614c865059dfdbd8e3cb681
kiran.gondi <kiran.gondi@planview.com>
2024-10-12 23:21:26 +0530

    Query String Vs Route Data

---
+++ Section06/ModelBindingExample/ModelBindingExample/Controllers/StoreController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace ModelBindingExample.Controllers
+{
+    public class StoreController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
