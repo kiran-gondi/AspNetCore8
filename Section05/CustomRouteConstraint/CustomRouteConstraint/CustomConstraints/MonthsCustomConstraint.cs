901c39567e96eb0e5b1bb8f24b2676fa1740835b
kiran.gondi <kiran.gondi@planview.com>
2024-10-12 15:26:04 +0530

    CustomRouteConstraint Class

---
+++ Section05/CustomRouteConstraint/CustomRouteConstraint/CustomConstraints/MonthsCustomConstraint.cs
@@ -0,0 +1,26 @@
+﻿
+using System.Text.RegularExpressions;
+
+namespace CustomRouteConstraint.CustomConstraints
+{
+    //Ex: sales-report/2030/apr
+    public class MonthsCustomConstraint : IRouteConstraint
+    {
+        public bool Match(HttpContext? httpContext, IRouter? route, string routeKey,
+            RouteValueDictionary values, RouteDirection routeDirection)
+        {
+            if (!values.ContainsKey(routeKey)) {
+                return false;
+            }
+
+            Regex monthRegex = new Regex($"^(apr|jul|oct|jan)$");
+            string? monthValue = Convert.ToString(values[routeKey]);
+
+            if (monthRegex.IsMatch(monthValue))
+            {
+                return true;
+            }
+            return false;
+        }
+    }
+}
