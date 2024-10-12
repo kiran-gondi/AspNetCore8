cbc055f3f7a3f410d3a8cdbed8aac065812462b1
kiran.gondi <kiran.gondi@planview.com>
2024-10-12 23:35:55 +0530

    Model Class

--- Section06/ModelBindingExample/ModelBindingExample/Controllers/HomeController.cs
+++ Section06/ModelBindingExample/ModelBindingExample/Controllers/HomeController.cs
@@ -1,4 +1,5 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using ModelBindingExample.Models;
 
 namespace ModelBindingExample.Controllers
 {
@@ -29,7 +30,7 @@ namespace ModelBindingExample.Controllers
             return Content($"Book id: {bookid}");
         }*/
 
-        [Route("bookstore/{bookid?}/{isLoggedin?}")]
+        /*[Route("bookstore/{bookid?}/{isLoggedin?}")]
         //URL: /bookstore?bookid=10&isloggedin=true
         //public IActionResult Index([FromRoute]int? bookid, [FromRoute] bool? isLoggedin)
         public IActionResult Index([FromQuery]int? bookid, [FromQuery] bool? isLoggedin)
@@ -55,6 +56,33 @@ namespace ModelBindingExample.Controllers
             }
 
             return Content($"Book id: {bookid}");
+        }*/
+
+        [Route("bookstore/{bookid?}/{isLoggedin?}")]
+        //URL: /bookstore?bookid=10&isloggedin=true
+        public IActionResult Index([FromQuery]int? bookid, [FromRoute]bool? isLoggedin, Book book)
+        {
+            if (bookid.HasValue == false)
+            {
+                return BadRequest("Book id is not supplied or empty");
+            }
+
+            if (bookid <= 0)
+            {
+                return BadRequest("Book id can't be less than or equal to 0");
+            }
+
+            if (bookid > 1000)
+            {
+                return NotFound("Book id can't be greater than 1000");
+            }
+
+            if (isLoggedin == false)
+            {
+                return StatusCode(401);
+            }
+
+            return Content($"Book id: {bookid}, Book: {book.ToString()}");
         }
     }
 }
