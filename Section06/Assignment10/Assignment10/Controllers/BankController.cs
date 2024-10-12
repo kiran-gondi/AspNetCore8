da58568b843d44bbac2fa1fb4e8e66d333c5e4a1
kiran.gondi <kiran.gondi@planview.com>
2024-10-12 23:02:21 +0530

    Assignment10

---
+++ Section06/Assignment10/Assignment10/Controllers/BankController.cs
@@ -0,0 +1,52 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Assignment10.Models;
+
+namespace Assignment10.Controllers
+{
+    public class BankController : Controller
+    {
+        [HttpGet]
+        [Route("/")]
+        public IActionResult Index()
+        {
+            return Content("Welcome to the Best Bank");
+        }
+
+        [HttpGet]
+        [Route("/account-details")]
+        public IActionResult Account()
+        {
+            Account account = new Account() { AccountNumber = 1001, AccountHolderName = "Bob", CurrentBalance = 5000 };
+            return Json(account);
+        }
+
+        [HttpGet]
+        [Route("/account-statement")]
+        public IActionResult AccountStatement()
+        {
+            return File("/dummy-pdf_2.pdf", "application/pdf");
+        }
+
+        [HttpGet]
+        [Route("/get-current-balance/{accountNumber:int?}")]
+        public IActionResult AccountBalance()
+        {
+            Account account = new Account() { AccountNumber = 1001, AccountHolderName = "Bob", CurrentBalance = 5000 };
+            int accountNumber = Convert.ToInt32(Request.RouteValues["accountNumber"]);
+
+            if(accountNumber == 0)
+            {
+                return NotFound("Account Number should be supplied");
+            }
+
+            if (account.AccountNumber == accountNumber)
+            {
+                return Ok(account.CurrentBalance);
+            }
+            else
+            {
+                return BadRequest($"Account Number should be {account.AccountNumber}");
+            }
+        }
+    }
+}
