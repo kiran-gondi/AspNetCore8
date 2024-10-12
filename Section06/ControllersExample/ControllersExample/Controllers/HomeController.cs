265f17ca58e547cca383ca9c616100e4565dacad
kiran.gondi <kiran.gondi@planview.com>
2024-10-12 16:40:38 +0530

    Controllers Example

---
+++ Section06/ControllersExample/ControllersExample/Controllers/HomeController.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace ControllersExample.Controllers
+{
+    public class HomeController : Controller
+    {
+        [Route("sayHello")]
+        public string method1()
+        {
+            return "Hello from method1";
+        }
+    }
+}
