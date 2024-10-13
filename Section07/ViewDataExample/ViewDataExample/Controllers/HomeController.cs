43fd41a6bf391b112bcfcfe3bef781c89e9e4256
kiran.gondi <kiran.gondi@planview.com>
2024-10-13 17:36:01 +0530

    Strongly Typed Views with Multiple Models

--- Section07/ViewDataExample/ViewDataExample/Controllers/HomeController.cs
+++ Section07/ViewDataExample/ViewDataExample/Controllers/HomeController.cs
@@ -8,6 +8,13 @@ namespace ViewDataExample.Controllers
     {
         private readonly ILogger<HomeController> _logger;
 
+        List<Person> people = new List<Person>()
+              {
+                  new Person() { Name = "John", DateOfBirth = DateTime.Parse("2000-05-06"), PersonGender = Gender.Male},
+                  new Person() { Name = "Linda", DateOfBirth = DateTime.Parse("2005-01-09"), PersonGender = Gender.Female},
+                  new Person() { Name = "Susan", DateOfBirth = null, PersonGender = Gender.Other}
+              };
+
         public HomeController(ILogger<HomeController> logger)
         {
             _logger = logger;
@@ -17,18 +24,38 @@ namespace ViewDataExample.Controllers
         {
             ViewData["appTitle"] = "Asp.Net Core Demo App!";
 
-            List<Person> people = new List<Person>()
-              {
-                  new Person() { Name = "John", DateOfBirth = DateTime.Parse("2000-05-06"), PersonGender = Gender.Male},
-                  new Person() { Name = "Linda", DateOfBirth = DateTime.Parse("2005-01-09"), PersonGender = Gender.Female},
-                  new Person() { Name = "Susan", DateOfBirth = DateTime.Parse("2008-07-12"), PersonGender = Gender.Other}
-              };
-
             //ViewData["people"] = people;
-            ViewBag.people = people;
-            return View();
+            //ViewBag.people = people;
+            //return View(people);
+            return View("Index", people);
         }
 
+        [Route("person-details/{name}")]
+        public IActionResult Details(string? name)
+        {
+            if(name == null)
+            {
+                return Content("Person name can't be null");
+            }
+
+           Person? matchPerson = people.Where(x=>x.Name == name).FirstOrDefault();
+           return View(matchPerson);//View/Home/Details.cshtml
+        }
+
+        [Route("person-with-product")]
+        public IActionResult PersonWithProduct() 
+        { 
+            Person p1 = new Person() { Name = "Linda1", DateOfBirth = DateTime.Parse("2005-01-09"), PersonGender = Gender.Female };
+            Product product1 = new Product() { ProductId = 101, ProductName = "Bluetooth" };
+
+            PersonAndProductWrapperModel personAndProductWrapperModel = new PersonAndProductWrapperModel()
+            {
+                PersonData = p1,
+                ProductData = product1,
+            };
+
+            return View("PersonAndProduct", personAndProductWrapperModel); 
+        }
         public IActionResult Privacy()
         {
             return View();
