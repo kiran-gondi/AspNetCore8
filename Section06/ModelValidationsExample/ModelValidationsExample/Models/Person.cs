18a30f239047229efe7c4c22ea5feb30e90b53f4
kiran.gondi <kiran.gondi@planview.com>
2024-10-13 13:49:29 +0530

    Collection Binding

--- Section06/ModelValidationsExample/ModelValidationsExample/Models/Person.cs
+++ Section06/ModelValidationsExample/ModelValidationsExample/Models/Person.cs
@@ -46,6 +46,8 @@ namespace ModelValidationsExample.Models
 
         public int? Age { get; set; }
 
+        public List<string?> Tags { get; set; } = new List<string?>();
+
         public override string ToString()
         {
             return $"Person Object - Person name: {PersonName}, Email: {Email}, " +
