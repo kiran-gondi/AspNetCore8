3ded7da02d09e14d3aec6f5b11a67078a9e42428
kiran.gondi <kiran.gondi@planview.com>
2024-10-13 20:34:31 +0530

    Layout Views Example

---
+++ Section09/LayoutViewsExample/LayoutViewsExample/Controllers/ProductsController.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace LayoutViewsExample.Controllers
+{
+    public class ProductsController : Controller
+    {
+        [Route("products")]
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        //URL: /search-products/1
+        [Route("search-products/{ProductID?}")]
+        public IActionResult Search(int? ProductID)
+        {
+            ViewBag.ProductID = ProductID;  
+            return View();
+        }
+
+        [Route("order-products")]
+        public IActionResult Order()
+        {
+            return View();
+        }
+    }
+}
