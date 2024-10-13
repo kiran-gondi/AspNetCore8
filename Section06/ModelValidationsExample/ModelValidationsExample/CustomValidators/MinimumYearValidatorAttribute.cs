ed0185b4389bf4d4acf012926d1b46c0842c78e6
kiran.gondi <kiran.gondi@planview.com>
2024-10-13 11:52:30 +0530

    Custom Validation

---
+++ Section06/ModelValidationsExample/ModelValidationsExample/CustomValidators/MinimumYearValidatorAttribute.cs
@@ -0,0 +1,38 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace ModelValidationsExample.CustomValidators
+{
+    public class MinimumYearValidatorAttribute : ValidationAttribute
+    {
+        public int MinimumYear { get; set; } = 2000;
+        public string DefaultErrorMessage { get; set; } = "Year should not be less than {0}";
+
+        public MinimumYearValidatorAttribute() { 
+        }
+
+        public MinimumYearValidatorAttribute(int minimumYear)
+        {
+            MinimumYear = minimumYear;
+        }
+
+        protected override ValidationResult? IsValid(object? value, ValidationContext validationContext)
+        {
+            if (value != null)
+            {
+                DateTime date = (DateTime)value;
+                if(date.Year >= MinimumYear)
+                //if(date.Year >= 2000)
+                {
+                    //return new ValidationResult("Minimum year allowed is 2000");
+                    return new ValidationResult(string.Format(ErrorMessage ?? DefaultErrorMessage, MinimumYear));
+                }
+                else
+                {
+                    return ValidationResult.Success;
+                }
+            }
+
+            return null;
+        }
+    }
+}
