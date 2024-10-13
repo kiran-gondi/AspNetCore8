04c2bee4a20537befc192343e8d49af00e21fb35
kiran.gondi <kiran.gondi@planview.com>
2024-10-13 16:52:32 +0530

    ViewData

---
+++ Section07/ViewDataExample/ViewDataExample/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace ViewDataExample.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
