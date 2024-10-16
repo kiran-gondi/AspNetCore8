5b95c872305484199c96f64487a15034b9bf412a
kiran.gondi <kiran.gondi@planview.com>
2024-10-16 20:03:49 +0530

    PartialViewExample + Cheatsheet

---
+++ Section10/PartialViewsExample/PartialViewsExample/Controllers/HomeController.cs
@@ -0,0 +1,37 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using PartialViewsExample.Models;
+
+namespace PartialViewsExample.Controllers
+{
+    public class HomeController : Controller
+    {
+        [Route("/")]
+        public IActionResult Index()
+        {
+            //ViewData["ListTitle"] = "Cities";
+            //ViewData["ListItems"] = new List<string>() { 
+            //    "Paris", "Newyork", "Navi Mumbai", "Rome"
+            //};
+            return View();
+        }
+
+        [Route("about")]
+        public IActionResult About()
+        {
+            return View();
+        }
+
+        [Route("programming-languages")]
+        public IActionResult ProgrammingLanguage()
+        {
+            ListModel listModel = new ListModel();
+            listModel.ListTitle = "Programming Languages List";
+            listModel.ListItems = new List<string>(){
+                "Phython", "Go", "C#" 
+            };
+
+            //return new PartialViewResult() { ViewName = "_ListParitalView", Model = listModel};
+            return PartialView("_ListPartialView", listModel);
+        }
+    }
+}
