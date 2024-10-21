3383224718b65ee51d4e5d2a3c1b5115867bc4d6
kiran.gondi <kiran.gondi@planview.com>
2024-10-21 19:52:21 +0530

    Assignment18-Completed

---
+++ Section10/Assignment18/Assignment18/Controllers/CityWeatherController.cs
@@ -0,0 +1,39 @@
+﻿using Assignment18.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Assignment18.Controllers
+{
+    public class CityWeatherController : Controller
+    {
+        List<CityWeather> cityWeatherList = new List<CityWeather>()
+        {
+            new CityWeather() { CityUniqueCode = "LDN", CityName = "London",
+                DateAndTime = Convert.ToDateTime("2030-01-01 8:00"),  TemperatureFahrenheit = 33 },
+            new CityWeather() { CityUniqueCode = "NYC", CityName = "London",
+                DateAndTime = Convert.ToDateTime("2030-01-01 3:00"),  TemperatureFahrenheit = 60 },
+            new CityWeather() { CityUniqueCode = "PAR", CityName = "Paris",
+                DateAndTime = Convert.ToDateTime("2030-01-01 9:00"),  TemperatureFahrenheit = 82 },
+
+        };
+
+        [HttpGet]
+        [Route("/")]
+        public IActionResult Index()
+        {
+            return View(cityWeatherList);
+        }
+        
+        [HttpGet]
+        [Route("/weather/{cityCode?}")]
+        public IActionResult CityWeather(string? cityCode)
+        {
+            if (string.IsNullOrEmpty(cityCode))
+            {
+                return View(new CityWeather());
+            }
+
+            CityWeather? cityWeather = cityWeatherList.Where(x=>x.CityUniqueCode == cityCode).FirstOrDefault();
+            return View(cityWeather);
+        }
+    }
+}
