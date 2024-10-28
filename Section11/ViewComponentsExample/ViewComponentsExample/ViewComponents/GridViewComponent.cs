46c10635f09c0c70cb58c32099deb14bca4778da
kiran.gondi <kiran.gondi@planview.com>
2024-10-28 22:16:35 +0530

    ViewComponents - Part 1 & 2

---
+++ Section11/ViewComponentsExample/ViewComponentsExample/ViewComponents/GridViewComponent.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace ViewComponentsExample.ViewComponents
+{
+    public class GridViewComponent : ViewComponent
+    {
+       public async Task<IViewComponentResult> InvokeAsync()
+        {
+            //return View(); //invoked a partial view Views/Shared/Components/Grid/Default.cshtml
+            return View("Default1");
+        }
+    }
+}
