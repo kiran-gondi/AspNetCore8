8238109847df8fa9657d2177c0b072b597ffc475
kiran.gondi <kiran.gondi@planview.com>
2024-10-29 23:04:51 +0530

    Without DI

---
+++ Section12/DIExample/DIExample/Controllers/HomeController.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Services;
+
+namespace DIExample.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly CitiesService _citiesService;
+
+        public HomeController()
+        {
+            _citiesService = new CitiesService();
+        }
+
+        [Route("/")]
+        public IActionResult Index()
+        {
+            List<string> cities = _citiesService.GetCities();
+            return View(cities);
+        }
+    }
+}
