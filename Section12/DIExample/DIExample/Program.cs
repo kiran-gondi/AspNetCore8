0004296d21245b62733dc626859d5e90c9be775e
kiran.gondi <kiran.gondi@planview.com>
2024-10-29 23:26:40 +0530

    DIP + IoC + DI

--- Section12/DIExample/DIExample/Program.cs
+++ Section12/DIExample/DIExample/Program.cs
@@ -1,5 +1,10 @@
+using ServiceContracts;
+using Services;
+
 var builder = WebApplication.CreateBuilder(args);
 builder.Services.AddControllersWithViews();
+
+builder.Services.Add(new ServiceDescriptor(typeof(ICitiesService), typeof(CitiesService), ServiceLifetime.Transient));
 
 var app = builder.Build();
 
