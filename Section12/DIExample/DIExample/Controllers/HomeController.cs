5f3157209f1fc65ec3998742bdbff2cb80d07ee6
kiran.gondi <kiran.gondi@planview.com>
2024-10-29 23:29:03 +0530

    DI - Method Injection

--- Section12/DIExample/DIExample/Controllers/HomeController.cs
+++ Section12/DIExample/DIExample/Controllers/HomeController.cs
@@ -7,16 +7,18 @@ namespace DIExample.Controllers
     public class HomeController : Controller
     {
         //private readonly CitiesService _citiesService;
-        private readonly ICitiesService _citiesService;
+        /*private readonly ICitiesService _citiesService;
 
+        //CONSTRUCTOR Injection
         public HomeController(ICitiesService citiesService)
         {
             //_citiesService = new CitiesService();
             _citiesService = citiesService;
-        }
+        }*/
+
 
         [Route("/")]
-        public IActionResult Index()
+        public IActionResult Index([FromServices]ICitiesService _citiesService)//METHOD Injection
         {
             List<string> cities = _citiesService.GetCities();
             return View(cities);
