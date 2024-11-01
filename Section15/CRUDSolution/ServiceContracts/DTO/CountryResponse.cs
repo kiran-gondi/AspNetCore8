ff2c59d83290a6d0115f31daa68b0ec6415b4e77
kiran.gondi <kiran.gondi@planview.com>
2024-11-02 00:53:18 +0530

    TDD for CountriesService

---
+++ Section15/CRUDSolution/ServiceContracts/DTO/CountryResponse.cs
@@ -0,0 +1,25 @@
+﻿using Entities;
+
+namespace ServiceContracts.DTO
+{
+    /// <summary>
+    /// DTO class that is used as return type for most of CoutnriesService methods.
+    /// </summary>
+    public class CountryResponse
+    {
+        public Guid CountryID { get; set; }
+        public string? CountryName { get; set; }
+    }
+
+    public static class CountryExtensions 
+    {
+        public static CountryResponse ToCountryResponse(this Country country)
+        {
+            return new CountryResponse
+            {
+                CountryID = country.CountryID,
+                CountryName = country.CountryName
+            };
+        }
+    }
+}
