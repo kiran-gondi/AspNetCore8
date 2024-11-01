a997b11271aca93bbada2455d357026a52687e90
kiran.gondi <kiran.gondi@planview.com>
2024-11-01 12:06:04 +0530

    Environment in Controller

--- Section13/EnvironmentsExample/EnvironmentsExample/Controllers/HomeController.cs
+++ Section13/EnvironmentsExample/EnvironmentsExample/Controllers/HomeController.cs
@@ -4,10 +4,21 @@ namespace EnvironmentsExample.Controllers
 {
     public class HomeController : Controller
     {
+        private readonly IWebHostEnvironment _webHostEnvironment;
+
+        public HomeController(IWebHostEnvironment webHostEnvironment)
+        {
+            _webHostEnvironment = webHostEnvironment;
+        }
+
         [Route("/")]
         [Route("some-route")]
         public IActionResult Index()
         {
+            //_webHostEnvironment.IsDevelopment()
+            //_webHostEnvironment.EnvironmentName;
+            ViewBag.CurrentEnvironment = _webHostEnvironment.EnvironmentName;
+            
             return View();
         }
 
--- Section13/EnvironmentsExample/EnvironmentsExample/Program.cs
+++ Section13/EnvironmentsExample/EnvironmentsExample/Program.cs
@@ -3,7 +3,11 @@ builder.Services.AddControllersWithViews();
 
 var app = builder.Build();
 
-if (app.Environment.IsDevelopment() || app.Environment.IsStaging() || app.Environment.IsEnvironment("Beta"))
+/*if (app.Environment.IsDevelopment() || app.Environment.IsStaging() || app.Environment.IsEnvironment("Beta"))
+{
+    app.UseDeveloperExceptionPage();
+}*/
+if (app.Environment.IsDevelopment())
 {
     app.UseDeveloperExceptionPage();
 }
