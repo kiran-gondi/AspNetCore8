0302ae5cfc3fc7dd6405f6d5b96f82d17bea8691
kiran.gondi <kiran.gondi@planview.com>
2024-11-02 12:28:27 +0530

    Add Country TDD

--- Section15/CRUDSolution/Services/CountriesService.cs
+++ Section15/CRUDSolution/Services/CountriesService.cs
@@ -1,13 +1,48 @@
-﻿using ServiceContracts;
+﻿using Entities;
+using ServiceContracts;
 using ServiceContracts.DTO;
 
 namespace Services
 {
     public class CountriesService : ICountriesService
     {
+        //private field
+        private readonly List<Country> _countries;
+
+        public CountriesService()
+        {
+            _countries = new List<Country>();
+        }
+
         public CountryResponse AddCountry(CountryAddRequest? countryAddRequest)
         {
-            throw new NotImplementedException();
+            //Validation: countryAddRequest parameter can't be null
+            if(countryAddRequest == null)
+            {
+                throw new ArgumentNullException(nameof(countryAddRequest));
+            }
+
+            //Validaton: countryName can't be null
+            if (countryAddRequest.CountryName == null) {
+                throw new ArgumentException(nameof(countryAddRequest.CountryName));
+            }
+
+            //Validation: CountryName can't be duplicate
+            if(_countries.Where(x=>x.CountryName == countryAddRequest.CountryName).Count() > 0)
+            {
+                throw new ArgumentException("Given country name already exists");
+            }
+
+            //Convert object from CountryAddRequest to Country type
+            Country country = countryAddRequest.ToCountry();
+
+            //generate CountryID
+            country.CountryID = Guid.NewGuid();
+
+            //Add country object into countries.
+            _countries.Add(country);
+
+            return country.ToCountryResponse();
         }
     }
 }
