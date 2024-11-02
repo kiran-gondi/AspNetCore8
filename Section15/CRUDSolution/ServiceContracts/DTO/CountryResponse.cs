1b8d48c4c66b789fea7818d51370d72ad4a17f36
kiran.gondi <kiran.gondi@planview.com>
2024-11-02 12:49:11 +0530

    TDD Get All Countries

--- Section15/CRUDSolution/ServiceContracts/DTO/CountryResponse.cs
+++ Section15/CRUDSolution/ServiceContracts/DTO/CountryResponse.cs
@@ -9,6 +9,28 @@ namespace ServiceContracts.DTO
     {
         public Guid CountryID { get; set; }
         public string? CountryName { get; set; }
+
+        //It compares the curent object to another object of CountryResponse type and returns true,
+        //if both values are same otherwise return false.
+        public override bool Equals(object? obj)
+        {
+            if(obj == null)
+            {
+                return false;
+            }
+
+            if(obj.GetType() != typeof(CountryResponse))
+            {
+                return false; 
+            }
+            CountryResponse country_to_compare = (CountryResponse)obj;
+            return this.CountryID == country_to_compare.CountryID && this.CountryName == country_to_compare.CountryName;
+        }
+
+        public override int GetHashCode()
+        {
+            return base.GetHashCode();
+        }
     }
 
     public static class CountryExtensions 
