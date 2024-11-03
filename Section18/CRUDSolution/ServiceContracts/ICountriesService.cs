b5618467ace6e24439a1200a0ccbb37157621d66
kiran.gondi <kiran.gondi@planview.com>
2024-11-03 17:37:00 +0530

    Initial CRUDSoln from Section-17

---
+++ Section18/CRUDSolution/ServiceContracts/ICountriesService.cs
@@ -0,0 +1,30 @@
+﻿using ServiceContracts.DTO;
+
+namespace ServiceContracts
+{
+    /// <summary>
+    /// Represents business logic for manipulating the Country entity.
+    /// </summary>
+    public interface ICountriesService
+    {
+        /// <summary>
+        /// Adds a country object to the list of countries
+        /// </summary>
+        /// <param name="countryAddRequest">Country object to add</param>
+        /// <returns>Returns the country object after adding it (including newly generated country id)</returns>
+        CountryResponse AddCountry(CountryAddRequest? countryAddRequest);
+
+        /// <summary>
+        /// Returns all countries from the list
+        /// </summary>
+        /// <returns>All countries from the list as list of CountryResponse</returns>
+        List<CountryResponse> GetAllCountries();
+
+        /// <summary>
+        /// Returns the country object based on the give country Id.
+        /// </summary>
+        /// <param name="CountryID">Country Id (guid) to search</param>
+        /// <returns>Matching country as CountryRespone object</returns>
+        CountryResponse? GetCountryByCountryId(Guid? CountryID); 
+    }
+}
---
+++ Section18/CRUDSolution/Services/CountriesService.cs
@@ -0,0 +1,77 @@
+﻿using Entities;
+using ServiceContracts;
+using ServiceContracts.DTO;
+
+namespace Services
+{
+    public class CountriesService : ICountriesService
+    {
+        //private field
+        private readonly List<Country> _countries;
+
+        public CountriesService(bool initialize = true)
+        {
+            _countries = new List<Country>();
+            if (initialize) 
+            {
+                _countries.AddRange(new List<Country>()
+                {
+                    new Country() { CountryID = Guid.Parse("387F0A4B-2C38-4ACD-AE8F-16917AD94B10"), CountryName = "USA" },
+                    new Country() { CountryID = Guid.Parse("25614871-FCBD-47F8-917F-A7302BDE4AC2"), CountryName = "Canada" },
+                    new Country() { CountryID = Guid.Parse("38FC6B05-95DE-491E-B045-F6D7ACB67EBD"), CountryName = "UK" },
+                    new Country() { CountryID = Guid.Parse("95B3D99D-1876-4C88-AFFA-3E6BCB328BA9"), CountryName = "India" },
+                    new Country() { CountryID = Guid.Parse("C3344753-92F8-40B1-BD0E-1CC0C502435A"), CountryName = "Australia" }
+                }); 
+            }
+        }
+
+        public CountryResponse AddCountry(CountryAddRequest? countryAddRequest)
+        {
+            //Validation: countryAddRequest parameter can't be null
+            if(countryAddRequest == null)
+            {
+                throw new ArgumentNullException(nameof(countryAddRequest));
+            }
+
+            //Validaton: countryName can't be null
+            if (countryAddRequest.CountryName == null) {
+                throw new ArgumentException(nameof(countryAddRequest.CountryName));
+            }
+
+            //Validation: CountryName can't be duplicate
+            if(_countries.Where(x=>x.CountryName == countryAddRequest.CountryName).Count() > 0)
+            {
+                throw new ArgumentException("Given country name already exists");
+            }
+
+            //Convert object from CountryAddRequest to Country type
+            Country country = countryAddRequest.ToCountry();
+
+            //generate CountryID
+            country.CountryID = Guid.NewGuid();
+
+            //Add country object into countries.
+            _countries.Add(country);
+
+            return country.ToCountryResponse();
+        }
+
+        public List<CountryResponse> GetAllCountries()
+        {
+            return _countries.Select(country => country.ToCountryResponse()).ToList();
+        }
+
+        public CountryResponse? GetCountryByCountryId(Guid? CountryID)
+        {
+            if (CountryID == null) { return null; }
+
+            Country? countryResponse = _countries.FirstOrDefault(temp => temp.CountryID == CountryID);
+
+            if (countryResponse == null)
+            {
+                return null;
+            }
+            return countryResponse.ToCountryResponse();
+        }
+    }
+}
