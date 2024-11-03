dc2c6359d726265c97b2a4b92847bfbd5c8f484b
kiran.gondi <kiran.gondi@planview.com>
2024-11-03 13:30:13 +0530

    IConfiguration in Controller

---
+++ Section14/ConfigurationExample/ConfigurationExample/Controllers/HomeController.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace ConfigurationExample.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly IConfiguration _configuration;
+
+        public HomeController(IConfiguration configuration)
+        {
+            _configuration = configuration;
+        }
+
+        [Route("/")]
+        public IActionResult Index()
+        {
+            ViewBag.MyKey = _configuration["MYkey"];
+            ViewBag.MyApiKey = _configuration.GetValue("MyApiKey", "the default key");
+            return View();
+        }
+    }
+}
