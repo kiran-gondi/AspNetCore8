23952a88966b357c0b3c495de5fc9eb0fcfd2054
kiran.gondi <kiran.gondi@planview.com>
2024-11-04 22:07:24 +0530

    Web Api Controllers with EF Core - Part 1, Part 2 & Part 3

---
+++ Section26/CitiesManager.WebApi/CitiesManager.WebApi/DatabaseContext/ApplicationDbContext.cs
@@ -0,0 +1,33 @@
+﻿using CitiesManager.WebApi.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace CitiesManager.WebApi.DatabaseContext
+{
+    public class ApplicationDbContext : DbContext
+    {
+        public ApplicationDbContext(DbContextOptions options) : base(options) 
+        {
+            
+        }
+
+        public ApplicationDbContext() { }
+
+        public virtual DbSet<City> Cities { get;set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+
+            modelBuilder.Entity<City>().HasData(new City()
+            {
+                CityID = Guid.Parse("E6B9AFB0-1249-45C2-8598-074D3A04FC34"),
+                CityName = "Delhi"
+            });
+            modelBuilder.Entity<City>().HasData(new City()
+            {
+                CityID = Guid.Parse("60D2F70D-27BD-4B73-9D9C-42C6A5686F20"),
+                CityName = "New York"
+            });
+        }
+    }
+}
--- Section26/CitiesManager.WebApi/CitiesManager.WebApi/Program.cs
+++ Section26/CitiesManager.WebApi/CitiesManager.WebApi/Program.cs
@@ -1,8 +1,15 @@
+using CitiesManager.WebApi.DatabaseContext;
+using Microsoft.EntityFrameworkCore;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 
 builder.Services.AddControllers();
+builder.Services.AddDbContext<ApplicationDbContext>(options =>
+{
+    options.UseSqlServer(builder.Configuration.GetConnectionString("Default"));
+});
 
 var app = builder.Build();
 
