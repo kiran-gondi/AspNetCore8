e6423e44ccbe49a6c9929da2addbbc78ff455652
kiran.gondi <kiran.gondi@planview.com>
2024-11-04 20:55:51 +0530

    Basic WebApi Project

---
+++ Section26/CitiesManager.WebApi/CitiesManager.WebApi/Controllers/TestController.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace CitiesManager.WebApi.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class TestController : ControllerBase
+    {
+        [HttpGet]
+        public string Method()
+        {
+            return "Hello";
+        }
+    }
+}
