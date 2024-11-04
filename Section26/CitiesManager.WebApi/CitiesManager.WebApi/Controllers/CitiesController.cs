6972d483e9feac3537ecad8e846488e89ec1e222
kiran.gondi <kiran.gondi@planview.com>
2024-11-04 22:07:24 +0530

    Web Api Controllers with EF Core - Part 1, Part 2 & Part 3

---
+++ Section26/CitiesManager.WebApi/CitiesManager.WebApi/Controllers/CitiesController.cs
@@ -0,0 +1,111 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using CitiesManager.WebApi.DatabaseContext;
+using CitiesManager.WebApi.Models;
+
+namespace CitiesManager.WebApi.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class CitiesController : ControllerBase
+    {
+        private readonly ApplicationDbContext _context;
+
+        public CitiesController(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+
+        // GET: api/Cities
+        [HttpGet]
+        public async Task<ActionResult<IEnumerable<City>>> GetCities()
+        {
+            return await _context.Cities.OrderBy(x=>x.CityName).ToListAsync();
+        }
+
+        // GET: api/Cities/5
+        [HttpGet("{id}")]
+        public async Task<ActionResult<City>> GetCity(Guid id)
+        {
+            var city = await _context.Cities.FindAsync(id);
+
+            if (city == null)
+            {
+                return NotFound();
+            }
+
+            return city;
+        }
+
+        // PUT: api/Cities/5
+        // To protect from overposting attacks, see https://go.microsoft.com/fwlink/?linkid=2123754
+        [HttpPut("{id}")]
+        public async Task<IActionResult> PutCity(Guid id, 
+            [Bind(nameof(City.CityID), nameof(City.CityName))] City city)
+        {
+            if (id != city.CityID)
+            {
+                return BadRequest();
+            }
+
+            //_context.Entry(city).State = EntityState.Modified;
+            var existingCity = await _context.Cities.FindAsync(id);
+
+            if (existingCity == null) {
+                return NotFound();
+            }
+            
+            existingCity.CityName = city.CityName;
+
+            try
+            {
+                await _context.SaveChangesAsync();
+            }
+            catch (DbUpdateConcurrencyException)
+            {
+                if (!CityExists(id))
+                {
+                    return NotFound();
+                }
+                else
+                {
+                    throw;
+                }
+            }
+
+            return NoContent();
+        }
+
+        // POST: api/Cities
+        // To protect from overposting attacks, see https://go.microsoft.com/fwlink/?linkid=2123754
+        [HttpPost]
+        public async Task<ActionResult<City>> PostCity([Bind(nameof(City.CityID), nameof(City.CityName))] City city)
+        {
+            _context.Cities.Add(city);
+            await _context.SaveChangesAsync();
+
+            return CreatedAtAction("GetCity", new { id = city.CityID }, city);
+        }
+
+        // DELETE: api/Cities/5
+        [HttpDelete("{id}")]
+        public async Task<IActionResult> DeleteCity(Guid id)
+        {
+            var city = await _context.Cities.FindAsync(id);
+            if (city == null)
+            {
+                return NotFound();
+            }
+
+            _context.Cities.Remove(city);
+            await _context.SaveChangesAsync();
+
+            return NoContent();
+        }
+
+        private bool CityExists(Guid id)
+        {
+            return _context.Cities.Any(e => e.CityID == id);
+        }
+    }
+}
---
+++ Section26/CitiesManager.WebApi/CitiesManager.WebApi/Migrations/20241104154735_Initial.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+#pragma warning disable CA1814 // Prefer jagged arrays over multidimensional
+
+namespace CitiesManager.WebApi.Migrations
+{
+    /// <inheritdoc />
+    public partial class Initial : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Cities",
+                columns: table => new
+                {
+                    CityID = table.Column<Guid>(type: "uniqueidentifier", nullable: false),
+                    CityName = table.Column<string>(type: "nvarchar(max)", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Cities", x => x.CityID);
+                });
+
+            migrationBuilder.InsertData(
+                table: "Cities",
+                columns: new[] { "CityID", "CityName" },
+                values: new object[,]
+                {
+                    { new Guid("60d2f70d-27bd-4b73-9d9c-42c6a5686f20"), "New York" },
+                    { new Guid("e6b9afb0-1249-45c2-8598-074d3a04fc34"), "Delhi" }
+                });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Cities");
+        }
+    }
+}
