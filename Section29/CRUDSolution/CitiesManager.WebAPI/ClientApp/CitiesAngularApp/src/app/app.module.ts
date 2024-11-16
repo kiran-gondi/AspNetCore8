af7295a111c5118f7892d6db803001e6214dc873
kiran.gondi <kiran.gondi@planview.com>
2024-11-17 01:15:15 +0530

    upgraded to .Net9.0 & Register Endpoint & Register UI

--- Section29/CRUDSolution/CitiesManager.WebAPI/ClientApp/CitiesAngularApp/src/app/app.module.ts
+++ Section29/CRUDSolution/CitiesManager.WebAPI/ClientApp/CitiesAngularApp/src/app/app.module.ts
@@ -7,12 +7,14 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { CitiesComponent } from './cities/cities.component';
 import { DisableControlDirective } from './directives/disable-control.directive';
+import { RegisterComponent } from './register/register.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     CitiesComponent,
-    DisableControlDirective
+    DisableControlDirective,
+    RegisterComponent
   ],
   imports: [
     BrowserModule,
