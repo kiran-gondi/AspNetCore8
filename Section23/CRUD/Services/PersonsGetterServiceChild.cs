cacb93d08ecdcc3f0e89e5d067394c385c58a2bb
kiran.gondi <kiran.gondi@planview.com>
2024-11-25 13:34:05 +0530

    Open/Closed Principle with interfaces & inheritance

---
+++ Section23/CRUD/Services/PersonsGetterServiceChild.cs
@@ -0,0 +1,57 @@
+﻿using Microsoft.Extensions.Logging;
+using OfficeOpenXml;
+using RepositoryContracts;
+using Serilog;
+using ServiceContracts.DTO;
+
+namespace Services
+{
+    public class PersonsGetterServiceChild : PersonsGetterService
+    {
+        private readonly IPersonsRepository _personsRepository;
+        private readonly ILogger<PersonsGetterService> _logger;
+        private readonly IDiagnosticContext _diagnosticContext;
+        public PersonsGetterServiceChild(IPersonsRepository personsRepository, 
+            ILogger<PersonsGetterService> logger, IDiagnosticContext diagnosticContext) 
+            : base(personsRepository, logger, diagnosticContext)
+        {
+            _personsRepository = personsRepository;
+        }
+
+        public async override Task<MemoryStream> GetPersonsExcel()
+        {
+            MemoryStream memoryStream = new MemoryStream();
+            using (ExcelPackage excelPackage = new ExcelPackage(memoryStream))
+            {
+                ExcelWorksheet workSheet = excelPackage.Workbook.Worksheets.Add("PersonsSheet");
+                workSheet.Cells["A1"].Value = "Person Name";
+                workSheet.Cells["B1"].Value = "Age";
+
+                using (ExcelRange headerCells = workSheet.Cells["A1:H1"])
+                {
+                    headerCells.Style.Fill.PatternType = OfficeOpenXml.Style.ExcelFillStyle.Solid;
+                    headerCells.Style.Fill.BackgroundColor.SetColor(System.Drawing.Color.LightGray);
+                    headerCells.Style.Font.Bold = true;
+                }
+
+                int row = 2;
+                List<PersonResponse> persons = await GetAllPersons();
+
+                foreach (PersonResponse person in persons)
+                {
+                    workSheet.Cells[row, 1].Value = person.PersonName;
+                    workSheet.Cells[row, 2].Value = person.Age;
+
+                    row++;
+                }
+
+                workSheet.Cells[$"A1:H{row}"].AutoFitColumns();
+
+                await excelPackage.SaveAsync();
+            }
+
+            memoryStream.Position = 0;
+            return memoryStream;
+        }
+    }
+}
