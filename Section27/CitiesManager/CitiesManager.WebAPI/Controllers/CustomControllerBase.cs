0893c19ac5ba67a5496b33c7e7d592fd1008e53e
Kiran Gondi <kiran.gondi@gmail.com>
2024-11-25 18:11:28 +0530

    Section27 (#32)
    * Added CS's
    
    * Swagger / OpenAPI - Basics
    
    * Documentation Comments
    
    * Content Negotiation
    
    * API Versions - Part1
    
    * API Versions - Part 2
    
    * API Versions - Part 3
    
    * Added Cheat Sheets
    
    ---------
    
    Co-authored-by: kiran.gondi <kiran.gondi@planview.com>

---
+++ Section27/CitiesManager/CitiesManager.WebAPI/Controllers/CustomControllerBase.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace CitiesManager.WebAPI.Controllers
+{
+[Route("api/v{version:apiVersion}/[controller]")] //UrlSegmentApiVersionReader
+//[Route("api/[controller]")] //QueryStringApiVersionReader
+[ApiController]
+ public class CustomControllerBase : ControllerBase
+ {
+ }
+}
+
