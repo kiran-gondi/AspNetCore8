ba2f46fc90470e508cbe01c6e879ec4ea1bfcebe
kiran.gondi <kiran.gondi@planview.com>
2024-11-25 22:03:23 +0530

    Repository Implementation - Part 1

---
+++ Section19/CRUD/RepositoryContracts/ICountriesRepository.cs
@@ -0,0 +1,26 @@
+﻿using Entities;
+
+namespace RepositoryContracts
+{
+    /// <summary>
+    /// Represents data access logic for mappng Person entity
+    /// </summary>
+    public interface ICountriesRepository
+    {
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="country"></param>
+        /// <returns></returns>
+        Task<Country> AddCountry(Country country);
+
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <returns></returns>
+        Task<List<Country>> GetAllCountries();
+
+        Task<Country?> GetCountryByCountryID(Guid countryID);
+        Task<Country?> GetCountryByCountryName(string countryName);
+    }
+}
