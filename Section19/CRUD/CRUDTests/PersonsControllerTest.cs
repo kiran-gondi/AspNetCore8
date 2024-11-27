cabbe92eedc7a3be16cb3b2cdfeac85187f85b04
kiran.gondi <kiran.gondi@planview.com>
2024-11-27 21:00:01 +0530

    Controller Unit Test - Part 1

---
+++ Section19/CRUD/CRUDTests/PersonsControllerTest.cs
@@ -0,0 +1,70 @@
+﻿using AutoFixture;
+using CRUDExample.Controllers;
+using FluentAssertions;
+using Microsoft.AspNetCore.Mvc;
+using Moq;
+using ServiceContracts;
+using ServiceContracts.DTO;
+using ServiceContracts.Enums;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace CRUDTests
+{
+    public class PersonsControllerTest
+    {
+        private readonly IPersonsService _personsService;
+        private readonly ICountriesService _countriesService;
+
+        private readonly Mock<IPersonsService> _personsServiceMock;
+        private readonly Mock<ICountriesService> _countriesServiceMock;
+
+        private readonly Fixture _fixture;
+
+        public PersonsControllerTest()
+        {
+            _fixture = new Fixture();
+
+            _countriesServiceMock = new Mock<ICountriesService>();
+            _personsServiceMock = new Mock<IPersonsService>();
+            
+            _countriesService = _countriesServiceMock.Object;
+            _personsService = _personsServiceMock.Object;
+        }
+
+        #region Index
+        [Fact]
+        public async Task Index_ShouldReturnIndexWithPersonsList()
+        {
+            //Arrange
+            List<PersonResponse> persons_response_list = _fixture.Create<List<PersonResponse>>();
+
+            PersonsController personsController = 
+                new PersonsController(_personsService, _countriesService);
+
+            _personsServiceMock
+                .Setup(temp => temp.GetFilteredPersons(It.IsAny<string>(), It.IsAny<string>()))
+                .ReturnsAsync(persons_response_list);
+
+            _personsServiceMock
+                .Setup(temp => temp.GetSortedPersons(It.IsAny<List<PersonResponse>>(), It.IsAny<string>(), It.IsAny<SortOrderOptions>()))
+                .ReturnsAsync(persons_response_list);
+
+
+            //Act
+            IActionResult result = await personsController.Index(_fixture.Create<string>(),
+                _fixture.Create<string>(),
+                _fixture.Create<string>(),
+                _fixture.Create<SortOrderOptions>());
+
+            //Assert
+            ViewResult viewResult = Assert.IsType<ViewResult>(result);
+
+            viewResult.ViewData.Model.Should().BeAssignableTo<List<PersonResponse>>();
+            viewResult.ViewData.Model.Should().Be(persons_response_list);
+        }
+        #endregion
+
+    }
+}
