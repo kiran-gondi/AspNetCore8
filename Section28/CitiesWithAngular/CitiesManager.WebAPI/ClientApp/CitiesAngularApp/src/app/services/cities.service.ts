ffeca77673f4e77c2b0f30f2ddf1e07405e80e2b
kiran.gondi <kiran.gondi@planview.com>
2024-11-28 17:37:55 +0530

    Angular POST

--- Section28/CitiesWithAngular/CitiesManager.WebAPI/ClientApp/CitiesAngularApp/src/app/services/cities.service.ts
+++ Section28/CitiesWithAngular/CitiesManager.WebAPI/ClientApp/CitiesAngularApp/src/app/services/cities.service.ts
@@ -3,6 +3,7 @@ import { City } from '../models/city';
 import { HttpClient, HttpHeaders } from '@angular/common/http';
 import { Observable } from 'rxjs';
 
+const API_BASE_URL: string = "https://localhost:7221/api";
 
 @Injectable({
   providedIn: 'root'
@@ -19,6 +20,14 @@ export class CitiesService {
     let headers = new HttpHeaders();
     headers = headers.append("Authorization", "Bearer token");
 
-    return this.httpClient.get<City[]>("https://localhost:7221/api/v1/cities", { headers: headers });
-  } 
+    return this.httpClient.get<City[]>(`${API_BASE_URL}/v1/cities`, { headers: headers });
+  }
+
+  public postCity(city: City): Observable<City> {
+    let headers = new HttpHeaders();
+    headers = headers.append("Authorization", "Bearer token");
+
+    return this.httpClient.post<City>(`${API_BASE_URL}/v1/cities`, city, { headers: headers });
+  }
+
 }
